b418b93cbd91d932951d79aaea7c3a0168e25b19
Thomas Lawless <tlawless94@gmail.com>
2018-03-07 14:44:07 -0600

    beginning of front end design

---
+++ client/components/grid.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+//creates a 50 X 50 Grid
+class Grid extends Component {
+  constructor(){
+    super()
+  }
+
+  //from https://github.com/react-dnd/react-dnd/blob/master/examples/00%20Chessboard/Tutorial%20App/Board.js
+  renderSquares(i){
+    const x = i % 8
+		const y = Math.floor(i / 8)
+
+		return (
+			<div key={i} style={{ width: '12.5%', height: '12.5%' }}>
+				<GridSquare x={x} y={y}>
+					{this.renderPiece(x, y)}
+				</GridSquare>
+			</div>
+		)
+  }
+
+}
